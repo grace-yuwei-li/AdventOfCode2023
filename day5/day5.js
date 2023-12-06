0711694deb0c1554edee55c8e3db12fce2ccc939
grace-yuwei-li <grace.yuwei.li@gmail.com>
2023-12-06 03:01:43 -0500

    Day 5 part 1 solution

--- day5/day5.js
+++ day5/day5.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 // Day 5
 // Part 1: What is the lowest location number that corresponds to any of the initial seed numbers?
-fs.readFile('day5input copy.txt', 'utf-8', (err, data) => {
+fs.readFile('day5input.txt', 'utf-8', (err, data) => {
 	if (err) {
 		console.err(
 			'please help me, it is only day 5 and im struggling so much',
@@ -12,100 +12,119 @@ fs.readFile('day5input copy.txt', 'utf-8', (err, data) => {
 
 	const lines = data.split('\n');
 	const seedMap = new Map();
-	const seedIDs = lines[0].slice(8).split(' ');
+	const seedIDs = lines[0].slice(7).split(' ').map(Number);
 
 	for (let i = 0; i < seedIDs.length; i++) {
-		seedMap.set(parseInt(seedIDs[i]), -1);
+		seedMap.set(parseInt(seedIDs[i]), parseInt(seedIDs[i]));
 	}
-
-	seedMap.forEach((value, key) => {
-		console.log('seed id: ', key, ' destination: ', value);
-	});
 
 	const sections = lines.slice(1).join('\n').replace(/\s+/, '').split('\n');
 
-	const seedToSoilMap = new Map();
-	const soilToFertilizerMap = new Map();
-	const fertilizerToWaterMap = new Map();
-	const waterToLightMap = new Map();
-	const lightToTempMap = new Map();
-	const tempToHumidityMap = new Map();
-	const humidityToLocationMap = new Map();
-
-	// const mapSections = {
-	// 	seedToSoilMap: seedToSoilMap,
-	// 	soilToFertilizerMap: soilToFertilizerMap,
-	// 	fertilizerToWaterMap: fertilizerToWaterMap,
-	// 	waterToLightMap: waterToLightMap,
-	// 	lightToTempMap: lightToTempMap,
-	// 	tempToHumidityMap: tempToHumidityMap,
-	// 	humidityToLocationMap: humidityToLocationMap,
-	// };
+	const seedToSoilArr = [];
+	const soilToFertilizerArr = [];
+	const fertilizerToWaterArr = [];
+	const waterToLightArr = [];
+	const lightToTempArr = [];
+	const tempToHumidityArr = [];
+	const humidityToLocationArr = [];
+	const arrSections = {
+		'seed-to-soil map:': seedToSoilArr,
+		'soil-to-fertilizer map:': soilToFertilizerArr,
+		'fertilizer-to-water map:': fertilizerToWaterArr,
+		'water-to-light map:': waterToLightArr,
+		'light-to-temperature map:': lightToTempArr,
+		'temperature-to-humidity map:': tempToHumidityArr,
+	};
 
 	let line = 0;
 	while (line < sections.length) {
-		if (sections[line].match('seed-to-soil map')) {
-			line++;
-			while (sections[line] !== '') {
-				parseMapData(sections[line], seedToSoilMap);
-				line++;
-			}
-		}
-		if (sections[line].match('soil-to-fertilizer map')) {
-			line++;
-			while (sections[line] !== '') {
-				parseMapData(sections[line], soilToFertilizerMap);
-				line++;
-			}
-		}
-		if (sections[line].match('fertilizer-to-water map')) {
-			line++;
-			while (sections[line] !== '') {
-				parseMapData(sections[line], fertilizerToWaterMap);
-				line++;
-			}
-		}
-		if (sections[line].match('water-to-light map')) {
-			line++;
-			while (sections[line] !== '') {
-				parseMapData(sections[line], waterToLightMap);
-				line++;
-			}
-		}
-		if (sections[line].match('light-to-temperature map')) {
-			line++;
-			while (sections[line] !== '') {
-				parseMapData(sections[line], lightToTempMap);
-				line++;
-			}
-		}
-		if (sections[line].match('temperature-to-humidity map')) {
-			line++;
-			while (sections[line] !== '') {
-				parseMapData(sections[line], tempToHumidityMap);
-				line++;
-			}
-		}
-		if (sections[line].match('humidity-to-location map')) {
+		const currentSection = sections[line];
+		const arr = arrSections[currentSection];
+		if (currentSection.match('humidity-to-location map')) {
 			line++;
 			while (line < sections.length) {
-				parseMapData(sections[line], humidityToLocationMap);
+				parseArrData(sections[line], humidityToLocationArr);
+				line++;
+			}
+		} else if (arr) {
+			line++;
+			while (sections[line] !== '') {
+				parseArrData(sections[line], arr);
 				line++;
 			}
 		}
 		line++;
 	}
-	console.log(seedToSoilMap);
-	console.log(soilToFertilizerMap);
-	console.log(fertilizerToWaterMap);
-	console.log(waterToLightMap);
-	console.log(lightToTempMap);
-	console.log(tempToHumidityMap);
-	console.log(humidityToLocationMap);
+
+	const destinationArrays = [
+		seedToSoilArr,
+		soilToFertilizerArr,
+		fertilizerToWaterArr,
+		waterToLightArr,
+		lightToTempArr,
+		tempToHumidityArr,
+		humidityToLocationArr,
+	];
+
+	function compareForDestinationArrays(seedMap, index) {
+		if (index === destinationArrays.length) {
+			return;
+		}
+		const destArr = destinationArrays[index];
+		seedMap.forEach((value, key) => {
+			compareRange(seedMap, key, value, destArr);
+		});
+		compareForDestinationArrays(seedMap, index + 1);
+	}
+
+	compareForDestinationArrays(seedMap, 0);
+
+	const result = findMinValue(seedMap);
+	console.log('Lowest location number: ', result);
 });
 
-function parseMapData(line, map) {
+function findMinValue(map) {
+	let minValue = Infinity;
+
+	for (const value of map.values()) {
+		minValue = Math.min(value, minValue);
+	}
+
+	return minValue;
+}
+
+function parseArrData(line, arr) {
 	const entries = line.split(' ').map(Number);
-	map.set(entries, -1);
-	console.log(line);
+	arr.push(entries);
+}
+
+function compareRange(map, key, value, arr) {
+	for (let i = 0; i < arr.length; i++) {
+		if (value <= arr[i][1] + arr[i][2] - 1 && value >= arr[i][1]) {
+			map.set(key, arr[i][0] + value - arr[i][1]);
+		}
+	}
+}
+
+function printMap(map) {
+	console.log('size: ', map.size);
+	map.forEach((value, key) => {
+		console.log('Key: ', key, 'Value: ', value);
+	});
+}
+
+function printArr(arr) {
+	console.log('length: ', arr.length);
+	arr.forEach((value, index) => {
+		console.log(
+			'index',
+			index,
+			'Destination: ',
+			value[0],
+			'Source: ',
+			value[1],
+			'Offset ',
+			value[2]
+		);
+	});
 }
