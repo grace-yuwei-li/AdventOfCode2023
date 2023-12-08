78e71493f4c2cdb7f1a076c31663666d742da62e
grace-yuwei-li <grace.yuwei.li@gmail.com>
2023-12-07 22:08:41 -0500

    Day 6 solution part 2

--- day6/day6.js
+++ day6/day6.js
@@ -9,16 +9,24 @@ fs.readFile('day6input.txt', 'utf-8', (err, data) => {
 	}
 
 	const lines = data.split('\n');
-	const part1 = lines[0].split(/\s+/);
-	const part2 = lines[1].split(/\s+/);
-	const times = part1.slice(1).map(Number);
-	const distances = part2.slice(1).map(Number);
+	const timePart = lines[0].split(/\s+/);
+	const distancePart = lines[1].split(/\s+/);
+
+	const times = timePart.slice(1).map(Number);
+	const distances = distancePart.slice(1).map(Number);
+
+	// Part 2: Longer race with one time and distance
+	const time = parseInt(timePart.slice(1).join(''));
+	const distance = parseInt(distancePart.slice(1).join(''));
 
 	let product = 1;
 	for (let i = 0; i < times.length; i++) {
 		product *= numOfWaysToWin(times[i], distances[i]);
 	}
+	const numOfWaysToBeatRecord = numOfWaysToWin(time, distance);
+
 	console.log('Product of number of ways to win is: ', product);
+	console.log('Number of ways to beat the record is: ', numOfWaysToBeatRecord);
 });
 
 function numOfWaysToWin(raceTime, recordTime) {
